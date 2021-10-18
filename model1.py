dbdc19973828c0fad6b7420c406e59952d0e4254
Robert Carruthers <keepitsurreal@gmail.com>
2021-10-18 12:28:34 +0100

    minor changes

--- model1.py
+++ model1.py
@@ -8,4 +8,11 @@ Created on Mon Oct 18 12:12:50 2021
 import os
 import sys
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+import copy
+
 os.chdir('C://Code/Kaggle/heart-failure')
